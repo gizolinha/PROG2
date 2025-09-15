a61560cdcab761f79ac44512d540c97e6114018d
Giovanna Sona <155585610+gizolinha@users.noreply.github.com>
2025-09-15 18:40:19 -0300

    Add files via upload

--- A1/gbv.c
+++ A1/gbv.c
@@ -1,3 +1,36 @@
 #include <stdio.h>
-#include <time.h>
 #include "gbv.h"
+
+//estrutura do superbloco
+typedef struct {
+    long qnt_documentos;
+    long int offset_dire;
+
+} superbloco;
+
+
+int gbv_create(const char *filename) {
+
+}
+int gbv_open(Library *lib, const char *filename) {
+
+}
+
+int gbv_add(Library *lib, const char *archive, const char *docname) {
+
+}
+int gbv_remove(Library *lib, const char *docname) {
+
+}
+
+int gbv_list(const Library *lib){
+
+}
+
+int gbv_view(const Library *lib, const char *docname) {
+
+}
+
+int gbv_order(Library *lib, const char *archive, const char *criteria) {
+    
+}
